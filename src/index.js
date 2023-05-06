182a499dd98f8725af33b863681b6683f800962d
Katarzyna Cependa <kasia.cependa@gmail.com>
2023-05-06 15:43:00 +0200

    Adding Notiflix info

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
+// import { createGallery } from './createGallery';
 
 const API_key = '36085372-0e054a65c2dad8200a3139bdc';
 const API_URL = 'https://pixabay.com/api/';
 
 const searchInputEl = document.querySelector('#search-form input');
 const searchButtonEl = document.querySelector('#search-form button');
+const galleryEl = document.querySelector('.gallery');
+const loadMoreButtonEl = document.querySelector('.load-more');
 
 const searchApi = async () => {
   const response = await axios.get(API_URL, {
@@ -24,7 +27,9 @@ const searchApi = async () => {
 const createGallery = () => {
   searchApi()
     .then(response => {
-      console.log(response.data.hits);
+      const totalHits = response.data.total;
+      Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
+
       if (response.data.hits.length === 0) throw new Error();
     })
     .catch(error => {
